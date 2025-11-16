7996069ceb49689b969e0306051cc5470c44bc51
shahzaibali-edu <shahzaibali.nbc@gmail.com>
2025-11-16 20:38:23 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ st.title("Simple BMI Calculator")
 
 # Input fields
 weight = st.number_input("Enter Weight (kg)", min_value=1.0, step=0.1)
-height = st.number_input("Enter Height (meters)", min_value=0.1, step=0.01)
+height = st.number_input("Enter Height (cm)", min_value=0.1, step=0.01)
 
 # Calculate button
 if st.button("Calculate BMI"):
     # Calculation
-    bmi = weight / (height ** 2)
+    bmi = weight / (height*100 ** 2)
     
     # Display BMI
     st.metric(label="Your BMI", value=f"{bmi:.2f}")
